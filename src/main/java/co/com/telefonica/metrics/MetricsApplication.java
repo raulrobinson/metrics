398051a1b0657bcaac1eae5d502ce24b8203a1fc
Raul Bolivar <raul.bolivar.ext@telefonica.com>
2023-02-15 17:34:24 -0500

    prometheus

--- src/main/java/co/com/telefonica/metrics/MetricsApplication.java
+++ src/main/java/co/com/telefonica/metrics/MetricsApplication.java
@@ -1,13 +1,27 @@
 package co.com.telefonica.metrics;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class MetricsApplication {
 
+	final static Logger logger = LoggerFactory.getLogger(MetricsApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(MetricsApplication.class, args);
+	}
+
+	@GetMapping("/something")
+	public ResponseEntity<String> createLogs() {
+		logger.warn("Just checking");
+		return ResponseEntity.ok().body("All Ok");
 	}
 
 }
